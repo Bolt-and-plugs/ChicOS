28f3ab63e8b1b44fc7950f262ea8226236be3e76
Felipe-gsilva <felipe.gsilva@protonmail.com>
2025-03-16 18:12:45 -0300

    starting agnostic type list

--- src/modules/utils/list.h
+++ src/modules/utils/list.h
@@ -22,7 +22,7 @@ struct List {
 List *new_list(i32 data_size);
 
 Node *alloc_node(List *L, void *d);
-Node *push_list(List *l, void *d);
+void push_list(List *l, void *d);
 void pop_list(List *l, i32 id);
 
 void clear_list(List *l);
