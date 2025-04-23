8a666838f23bc4e837fe708c8477c922ec32b413
Felipe-gsilva <felipe.gsilva@protonmail.com>
2025-04-23 16:56:43 -0300

    fix: linting

--- src/modules/cpu/cpu.c
+++ src/modules/cpu/cpu.c
@@ -1,47 +1,42 @@
+#include "../io/file.h"
+#include "../log/log.h"
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
-#include "../log/log.h"
-#include "../file/file.h"
 #define TIME_SLICE 10 // time slice duration in ms
 
-void exec_program(file_buffer *sint){
-    char *command, aux[16];
-    int time
-    if(sint->fp == NULL){
-        printf("File not open properly!");
-        return;
+void exec_program(file_buffer *sint) {
+  char *command, aux[16];
+  int time;
+  if (sint->fp == NULL) {
+    printf("File not open properly!");
+    return;
+  }
+  for (int i = 0; i < 6; i++)
+    fgets(aux, sizeof(aux), sint->fp);
+  while (!feof(sint->fp)) {
+    fgets(aux, sizeof(aux), sint->fp);
+    command = strtok(aux, " ");
+    time = atoi(strtok(NULL, " "));
+    if (strcmp(command, "exec") == 0) {
+      printf("Executing program for %dms...", time);
+      sleep(time / 1000);
+    } else if (strcmp(command, "write") == 0) {
+      printf("Writing on dik for %dms...", time);
+      sleep(time / 1000);
+    } else if (strcmp(command, "read") == 0) {
+      printf("Reading on dik for %dms...", time);
+      sleep(time / 1000);
+    } else if (strcmp(command, "P") == 0) {
+
+    } else if (strcmp(command, "V") == 0) {
+
+    } else if (strcmp(command, "print") == 0) {
+
+    } else {
+      c_error(DISK_FILE_READ_ERROR, "Found invalid command!");
+      return;
     }
-    for(int i = 0; i < 6; i++)
-        fgets(aux, sizeof(aux), sint->fp);
-    while(!feof(sint->fp)){
-        fgets(aux, sizeof(aux), sint->fp);
-        command = strtok(aux, " ")
-        time = atoi(strok(NULL, " "))
-        if(strcmp(command, "exec") == 0){
-            printf("Executing program for %dms...", time)
-            sleep(time / 1000);
-        }
-        else if(strcmp(command, "write") == 0){
-            printf("Writing on dik for %dms...", time)
-            sleep(time / 1000);
-        }
-        else if(strcmp(command, "read") == 0){
-            printf("Reading on dik for %dms...", time)
-            sleep(time / 1000);
-        }
-        else if(strcmp(command, "P") == 0){
-
-        }
-        else if(strcmp(command, "V") == 0){
-
-        }
-        else if(strcmp(command, "print") == 0){
-
-        }
-        else {
-            c_error(DISK_FILE_READ_ERROR, "Found invalid command!")
-        }
-    }
+  }
 }
