a792af4cef48c789a8ee405c7e0afab955b3f4ac
Felipe-gsilva <felipe.gsilva@protonmail.com>
2025-04-26 09:43:50 -0300

    fix: reading char and alloc problem fixed

--- src/modules/io/file.c
+++ src/modules/io/file.c
@@ -4,21 +4,35 @@
 
 void read_header(file_buffer *fb) {
   file_header *header = alloc(sizeof(file_header));
-  char s[256];
+  char c;
+  int i = 0;
+  char aux[256];
 
-  fscanf(fb->fp, "%s", header->name);
+  fscanf(fb->fp, "%s", aux);
+  if (strlen(aux) >= 127) {
+    strncpy(header->name, aux, 127);
+    header->name[127] = '\0';
+  } else
+    strcpy(header->name, aux);
+
   fscanf(fb->fp, "%d", &header->seg_flag);
   fscanf(fb->fp, "%d", &header->priority);
   fscanf(fb->fp, "%d", &header->seg_size);
-  fscanf(fb->fp, "%s", s);
 
-  while(strlen(s) <= 1) {
-    strcat(header->semaphores,  s);
-    fscanf(fb->fp, "%s", s);
+  // has to be done twice in order to skip\n
+  c = fgetc(fb->fp);
+  c = fgetc(fb->fp);
+
+  while (c != '\n') {
+    if (c != ' ')
+      header->semaphores[i++] = c;
+    c = fgetc(fb->fp);
   }
-  printf("%s\n", header->semaphores);
 
-  //fb->h->rw_count = counting_io_operations(fb);
+  header->semaphores[i] = '\0';
+
+  fb->h = header;
+  fb->h->rw_count = counting_io_operations(fb);
 }
 
 i32 counting_io_operations(file_buffer *fb) {
