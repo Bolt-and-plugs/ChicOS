582cd00f14d543e9ca774162798ae4fa7ce764b6
Felipe-gsilva <felipe.gsilva@protonmail.com>
2025-05-04 15:56:34 -0300

    fix: leaks

--- src/modules/semaphore/semaphore.c
+++ src/modules/semaphore/semaphore.c
@@ -1,6 +1,7 @@
 #include "semaphore.h"
 #include "../../chicos.h"
 #include "../log/log.h"
+#include "../memory/mem.h"
 
 extern App app;
 
@@ -21,39 +22,39 @@ void semaphoreV(sem_t *s) {
 }
 
 void init_semaphore_list() {
-  app.semaphores = (semaphore_list*) malloc(sizeof(semaphore_list)); 
+  app.semaphores = (semaphore_list *)c_alloc(sizeof(semaphore_list));
   if (!app.semaphores) {
     c_error(SEMAPHORE_INIT_ERROR, "Failed to initialize semaphore list");
     return;
   }
   app.semaphores->last = 0;
   for (int i = 0; i < MAX_SIZE_SEMAPHORES; i++) {
-    app.semaphores->l[i]->id = 0;
-    app.semaphores->l[i]->nome ='/0';
-    app.semaphores->l[i]->s = NULL;
+    app.semaphores->l[i].id = 0;
+    app.semaphores->l[i].nome = '\0';
+    app.semaphores->l[i].s = NULL;
   }
 }
 
 void init_semaphore(char nome) {
   for (int i = 0; i < MAX_SIZE_SEMAPHORES; i++)
-    if (app.semaphores->l[i]->nome == nome) {
+    if (app.semaphores->l[i].nome == nome) {
       c_error(SEMAPHORE_INIT_ERROR, "Semaphore already exists");
       return;
     }
-  
+
   if (app.semaphores->last >= MAX_SIZE_SEMAPHORES) {
     c_error(SEMAPHORE_INIT_ERROR, "Semaphore list is full");
     return;
   }
 
-  semaphore *sem = malloc(sizeof(semaphore));
+  semaphore *sem = c_alloc(sizeof(semaphore));
 
   sem->nome = nome;
   sem->id = app.cpu.quantum_time;
-  sem->s = (sem_t*) malloc(sizeof(sem_t));
+  sem->s = (sem_t *)c_alloc(sizeof(sem_t));
 
   sem_init(sem->s, 0, 1);
 
-  app.semaphores->l[app.semaphores->last] = sem;
+  app.semaphores->l[app.semaphores->last] = *sem;
   app.semaphores->last++;
 }
--- src/modules/semaphore/semaphore.h
+++ src/modules/semaphore/semaphore.h
@@ -1,17 +1,17 @@
 #ifndef _SEMAPHORE
 #define _SEMAPHORE
 #define MAX_SIZE_SEMAPHORES 32
+#include "../../defines.h"
 #include <semaphore.h>
 
-
 typedef struct __semaphore {
-  sem_t* s;
+  sem_t *s;
   char nome;
   u16 id;
 } semaphore;
 
 typedef struct __semaphore_list {
-  semaphore *l[MAX_SIZE_SEMAPHORES];
+  semaphore l[MAX_SIZE_SEMAPHORES];
   int last;
 } semaphore_list;
 
