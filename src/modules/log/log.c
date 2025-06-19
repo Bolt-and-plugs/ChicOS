53fa0ad5e76503376deddf99184fa89ac03f3078
Felipe Gomes da Silva <felipe.gsilva@protonmail.com>
2025-06-19 17:32:51 -0300

    better logging

--- src/modules/log/log.c
+++ src/modules/log/log.c
@@ -17,64 +17,65 @@ void c_log(log_level level, status_code status, const char *str, ...) {
     return;
 
   if (!app.rdr.active) {
+    time_t clk = time(NULL);
 
-    puts("\n");
+    printf("\n%-10s", sanitize_str(ctime(&clk) + 11));
     switch (level) {
     case ERROR:
-      printf("[\033[31;1;4mERROR\033[0m ");
+      printf("\033[31;1;4mERROR\033[0m ");
       break;
     case WARN:
-      printf("[\033[93;1;4mWARN\033[0m ");
+      printf("\033[93;1;2mWARN\033[0m ");
       break;
     case INFO:
-      printf("[\033[1;37;4mINFO\033[0m ");
+      printf("\033[1;94;1mINFO\033[0m ");
       break;
     case DEBUG:
-      printf("[\033[94;1;4mDEBUG\033[0m ");
+      printf("\033[1;95;4mDEBUG\033[0m ");
       break;
     case CRIT_ERROR:
-      printf("[\033[31;1;4mCRIT_ERROR\033[0m ");
+      printf("\033[31;1;4mCRIT_ERROR\033[0m ");
       break;
     default:
       break;
     }
 
     if ((status / 100) != -1) {
-      printf("STATUS CODE: ");
+      printf("| STATUS CODE: ");
     }
     switch (status / 100) {
     case -1:
       break;
     case 0:
-      printf("| %d - SCHEDULER ", status);
+      printf("%d - SCHEDULER |", status);
       break;
     case 1:
-      printf("| %d - Process Handling Context | ", status);
+      printf("%d - Process Handling Context | ", status);
       break;
     case 2:
-      printf("| %d - CPU Related Context | ", status);
+      printf("%d - CPU Related Context | ", status);
       break;
     case 3:
-      printf("| %d - Memory Status Context | ", status);
+      printf("%d - Memory Status Context | ", status);
       break;
     case 5:
-      printf("| %d - User Status Context | ", status);
+      printf("%d - User Status Context | ", status);
       break;
     default:
-      printf("| %d - Unknown Status | ", status);
+      printf("%d - Unknown Status | ", status);
     }
   }
 
-  char buffer[4096];
-
+  char *buffer;
+  buffer = calloc(sizeof(char), 4096);
   va_list arg_list;
   va_start(arg_list, str);
   vsnprintf(buffer, 255, str, arg_list);
   va_end(arg_list);
-  time_t clk = time(NULL);
-  if (!app.rdr.active) {
-    printf("%s]: %s\n", sanitize_str(ctime(&clk)), sanitize_str(buffer));
-  }
+  if (!app.rdr.active)
+    printf("%s\n", sanitize_str(buffer));
+
+  free(buffer);
 
   if (app.rdr.active && app.debug)
     render_log(buffer);
